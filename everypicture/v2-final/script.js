4b6b418f2d0b9d2a95bd164450b5e46c83d0eeb5
Alejandra Gonzalez Manzo <alejandraaagonzalez26@gmail.com>
2026-02-24 11:10:22 -0800

    changed name

--- everypicture/v2-final/script.js
+++ everypicture/v2-final/script.js
@@ -2,7 +2,7 @@
     'use strict';
     console.log ('reading js');
     
-    const myImages = ['sketch2.jpg', 'purse.jpg', 'coffee.jpg', 'makeup.jpg', 'room.jpg'];
+    const myImages = ['sketch2.jpg', 'purse.JPG', 'coffee.JPG', 'makeup.JPG', 'room.JPG'];
 
     const descriptions = [
         "The objects that every designer carries with them. It holds the story of things that are most important to them. Few objects that spark that creativity within them.",
